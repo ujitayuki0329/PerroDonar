6a7a17da9b3966b5387c82abad2626af1ba59677
[ujitayuki0329] <[ujitayuki0329@gmail.com]>
2022-07-03 16:28:36 +0900

    開発環境構築

---
+++ components/WelcomeBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card
+    color="red accent-1"
+    flat
+    height="100%"
+    tile
+  >
+    <div class="welcome-bar">
+      <h1>userさん ようこそPerroDonarへ</h1>
+    </div>
+  </v-card>
+</template>
+
+<style scoped>
+.welcome-bar{
+  text-align: center;
+}
+.welcome-bar h1 {
+  font-size: 16px;
+  color: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <WelcomeBar />
+  </div>
+</template>
+
+<script>
+  import Header from '~/components/Header.vue'
+  import WelcomeBar from '~/components/WelcomeBar.vue'
+  export default {
+    components: {
+      Header,
+      WelcomeBar,
+    },
+    data () {
+      return {
+        
+      }
+    },
+  }
+</script>
